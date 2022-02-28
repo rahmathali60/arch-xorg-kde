3f93e3f02c6ee2563adfd23420c86d02d2de85b9
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-02-28 15:55:36 +0530

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -15,26 +15,25 @@ echo root:0985 | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet libvirt dialog wpa_supplicant mtools bluez bluez-utils dosfstools reflector base-devel linux-headers avahi gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils bash-completion openssh rsync reflector acpi acpi_call tlp bridge-utils dnsmasq vi pulseaudio ipset flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font xdg-user-dirs xdg-utils pulseaudio pavucontrol 
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog kdialog  wpa_supplicant mtools bluez=qt bluez-utils dosfstools reflector base-devel linux-lts-headers avahi bash-completion openssh rsync reflector acpi acpi_call-lts tlp bridge-utils dnsmasq pulseaudio ipset sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font xdg-user-dirs xdg-utils pulseaudio pavucontrol 
 
 # pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
-syst
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
-useradd -m alone
-echo alone:0985 | chpasswd
-usermod -aG libvirt alone
+useradd -m rahmath
+echo rahmath:0985 | chpasswd
+#usermod -aG libvirt rahmath
 
 echo "alone ALL=(ALL) ALL" >> /etc/sudoers.d/alone
 
