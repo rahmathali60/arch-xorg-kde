d38183db450a6e74722f379cca90feaa0ef3ec72
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2022-02-28 16:30:12 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -2,15 +2,14 @@
 
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-
+sudo pacman -Syy
 #sudo reflector -c Switzerland -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
 
 #sudo systemctl enable --now auto-cpufreq
 
-
-sudo pacman -S --noconfirm xorg sddm plasma libreoffice-fresh ark dolphin dolphin-plugins sweeper konsole kate okular gwenview kcalc gnome-icon-theme gnome-themes-extra firefox htop vlc powertop adobe-source-sans-pro-fonts ttf-liberation noto-fonts noto-fonts-emoji adobe-source-han-serif-otc-fonts
+sudo pacman -S sddm systemsettings xdg-desktop-portal-kde sddm-kcm powerdevil polkit-kde-agent plasma-workspace plasma-systemmonitor plasma-pa plasma-nm plasma-integration plasma-desktop plasma-browser-integration milou libksysguard libkscreen kwrited kwin kwallet-pam ksystemstats ksshaskpass kscreenlocker kscreen kmenuedit kinfocenter khotkeys kgamma5 kdeplasma-addons kdecoration kde-gtk-config kde-cli-tools kactivitymanagerd drkonqi breeze-gtk breeze bluedevil plasma-disks ark dolphin dolphin-plugins pacman-contrib sweeper konsole spectacle kate okular gwenview kcalc gnome-themes-extra ttf-liberation firefox htop vlc ttf-liberation neofetch htop 
 sudo systemctl enable sddm
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-reboot
+#/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+#sleep 5
+#reboot
